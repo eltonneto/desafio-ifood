d2e540b91b45422abbdc3a57843bf3582f6c41f4
eltonneto <eltonsilva.ti@gmail.com>
2024-01-06 16:07:51 -0300

    desafio

--- hello_world.js
+++ hello_world.js
@@ -1,2 +1,10 @@
-console.log("Olá mundo!")
-console.log("Desafio feito.")
+let nome_heroi
+let xp_heroi
+
+nome_heroi = prompt("Nome do Herói: ")
+xp_heroi = prompt("XP do Herói: ")
+
+if (xp_heroi < 1000) {
+    console.log("O Herói de nome **{nome_heroi}** está no nível de **{xp_heroi}")
+
+}
